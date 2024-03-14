7d8d0f20fdffa13b8d3b92f72d59740d5b31a4d9
LANH27 <aleis720221.siul@gmail.com>
2024-03-14 12:19:40 -0600

    ⭐Feat:Eventos de Usuario Crea el boton para agreagar nuevos items a la lista

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,21 @@ const items = ref([
   {id: 2, label:'Carne'},
   {id: 3, label:'Pan'},
   {id: 4, label:'Galletas'}
+
+
 ]);
 </script>
 
 
 <template>
   <h1> <i v-bind:class="shoppingIcon">local_mall</i> {{ header }}</h1>
+  <div class="add-item form">
   <input v-model="newItem" type="text"placeholder="Agregar articulo">
+  <!-- Checkbox -->
+  <label><input type="checkbox" v-model="newItemHighPriority">Alta Prioridad</label>
+  <!-- Boton-->
+  <button class="btn btn-primary">Agregar Articulo</button>
+  </div>
   <ul>
     <li v-for="item in items" v-bind:key="item.id">⭐{{ item.label }}</li>
   </ul>
