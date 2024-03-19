be6f6db71844e8e8657fddcf87a02d16ed3db246
LANH27 <aleis720221.siul@gmail.com>
2024-03-19 11:17:25 -0600

    ⭐Feat:V-else

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,77 @@
 <script setup>
-//Importando funcion para crear referencias reactivas
+// Importando funcion
+// para crear referencias reactivas
 import { ref } from 'vue'
-//Creando una referencia de tipo string
-const header = ref('App Lista de compras');
+// Creando una referencia reactiva
+// de tipo string
+const header = ref('App lista de compras');
 const shoppingIcon = ref('material-icons shopping-cart-icon');
-//Creando un referencia reactiva para almacenar el valor de la lista
+// Creando una referencia reactiva
+// para alamacenar el valor de la lista
 const items = ref([
-  {id: 0, label:'Leche'},
-  {id: 1, label:'Arroz'},
-  {id: 2, label:'Carne'},
-  {id: 3, label:'Pan'},
-  {id: 4, label:'Galletas'}
-])
+  // { id: 0, label: 'Leche' },
+  // { id: 1, label: 'Arroz' },
+  // { id: 2, label: 'Carne' },
+  // { id: 3, label: 'Pan' },
+  // { id: 4, label: 'Huevos' }
+]);
 const newItem = ref('');
-const newItemHighPriority = ref (false)
-//Metodos
+const newItemHighPriority = ref(false)
+// Metodos
 const saveItems = () => {
-  //Agrega un nuevo elemento a la lista proveniente de la caja de texto 
-  items.value.push({ id: items.value.length, label: newItem.value})
-  //Borramos el contenido de la caja de texto
+  // Agrega un nuevo elemento a la lista
+  // proveniente de la caja de texto
+  items.value.push({ id: items.value.length, label: newItem.value })
+  // Borramos el contenido de la caja de texto
   newItem.value = "";
 };
+const doEdit = (edit) => {
+  editing.value = edit;
+  newItem.value = "";
+}
+const editing = ref(false);
 </script>
 
-
 <template>
-  <h1> <i v-bind:class="shoppingIcon">local_mall</i> {{ header }}</h1>
-
-  <form v-on:submit.prevent="items.push({ id: items.length, label: newItem})" class="add-item form">
-  <form v-on:submit.prevent="saveItems" class="add-item form">
-
-    <input v-model="newItem" type="text" placeholder="Agregar Articulo">
-
-    <label><input type="checkbox" v-model="newItemHighPriority">Alta Prioridad</label>
-    
+  <!-- Header -->
+  <div class="header">
+    <h1>
+      <i :class="shoppingIcon">local_mall</i> {{ header }}
+    </h1>
+    <button 
+      v-on:click="doEdit(false)"
+      v-if="editing" 
+      class="btn">
+      Cancelar
+    </button>
+    <button 
+      v-else
+      v-on:click="doEdit(true)" 
+      class="btn btn-primary">
+      Agregar Articulo
+    </button>
+  </div>
+  <!-- Formulario -->
+  <form v-if="editing" v-on:submit.prevent="saveItems" class="add-item form">
+    <input v-model="newItem" type="text" placeholder="Agregar articulo">
+    <!-- Checkbox -->
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority">
+      Alta Prioridad
+    </label>
+    <!-- Boton -->
     <button class="btn btn-primary">
-    Agrear Articulo</button>
-    </form>
+      Agregar Articulo
+    </button>
   </form>
+  <!-- Entrega de lista -->
   <ul>
-    <li v-for="({id, label}, i) in items" v-bind:key="id">⭐{{ label }}</li>
+    <li v-for="{ id, label } in items" v-bind:key="id">⭐ {{ label }}</li>
   </ul>
+  <!-- Mensaje condicional -->
+  <p v-if="items.length === 0">🥀 No hay elementos en la lista 🥀</p>
 </template>
+
 <style scoped>
 .shopping-cart-icon {
   font-size: 2rem;
