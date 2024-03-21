a7acad3c753d9527be026e1b3d988d3c13530f7c
LANH27 <aleis720221.siul@gmail.com>
2024-03-21 12:21:35 -0600

    ⭐Feat:Highpriority

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,14 @@ const shoppingIcon = ref('material-icons shopping-cart-icon');
 // Creando una referencia reactiva
 // para alamacenar el valor de la lista
 const items = ref([
- { id: 0, label: 'Leche', purchased:false, highPriority:true },
- { id: 1, label: 'Arroz', purchased:false, highPriority:false },
- { id: 2, label: 'Carne', purchased:true, highPriority:true },
- { id: 3, label: 'Pan', purchased:false, highPriority:false },
- { id: 4, label: 'Huevos', purchased:true, highPriority:true }
+//  { id: 0, label: 'Leche', purchased:false, highPriority:true },
+//  { id: 1, label: 'Arroz', purchased:false, highPriority:false },
+//  { id: 2, label: 'Carne', purchased:true, highPriority:true },
+//  { id: 3, label: 'Pan', purchased:false, highPriority:false },
+//  { id: 4, label: 'Huevos', purchased:true, highPriority:true }
 ]);
 const togglePurchased = (item) => {
-  item.purchased = !item.purchased 
+  item.purchased = !item.purchased
  };
 const newItem = ref('');
 const newItemHighPriority = ref(false)
@@ -24,13 +24,19 @@ const newItemHighPriority = ref(false)
 const saveItems = () => {
   // Agrega un nuevo elemento a la lista
   // proveniente de la caja de texto
-  items.value.push({ id: items.value.length, label: newItem.value })
+  items.value.push(
+    { id: items.value.length
+      , label: newItem.value,
+       highPriority: newItemHighPriority.value
+    })
   // Borramos el contenido de la caja de texto
   newItem.value = "";
+  newItemHighPriority.value=false;
 };
 const doEdit = (edit) => {
   editing.value = edit;
   newItem.value = "";
+  newItemHighPriority.value=false;
 }
 const editing = ref(false);
 </script>
@@ -41,15 +47,15 @@ const editing = ref(false);
     <h1>
       <i :class="shoppingIcon">local_mall</i> {{ header }}
     </h1>
-    <button 
+    <button
       v-on:click="doEdit(false)"
-      v-if="editing" 
+      v-if="editing"
       class="btn">
       Cancelar
     </button>
-    <button 
+    <button
       v-else
-      v-on:click="doEdit(true)" 
+      v-on:click="doEdit(true)"
       class="btn btn-primary">
       Agregar Articulo
     </button>
@@ -69,20 +75,20 @@ const editing = ref(false);
   </form>
   <!-- Entrega de lista -->
   <ul>
-    <li 
+    <li
 
-    v-for="({ id, label, purchased, highPriority}, index) in items" 
-    @click="togglePurchased(items[index])"  
-    :class="{priority:highPriority, strikeout:purchased}" 
+    v-for="({ id, label, purchased, highPriority}, index) in items"
+    @click="togglePurchased(items[index])"
+    :class="{priority:highPriority, strikeout:purchased}"
     v-bind:key="id">⭐ {{ label }}</li>
   </ul>
-  
+
    <!-- <ul>
-    <li v-for="{ id, label, purchased, highPriority} in items"   
-    :class="{purchased ?'strikeout': '',highPriority ? 'prioryty' :''}" 
+    <li v-for="{ id, label, purchased, highPriority} in items"
+    :class="{purchased ?'strikeout': '',highPriority ? 'prioryty' :''}"
     v-bind:key="id">
     ⭐ {{ label }}</li>
-  </ul> --> 
+  </ul> -->
   <!-- Mensaje condicional -->
   <p v-if="items.length === 0">🥀 No hay elementos en la lista 🥀</p>
 </template>
